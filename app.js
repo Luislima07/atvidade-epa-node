f0d28a7d4bd20f4bffb113c95e6e1745694b31d9
Luislima07 <161834927+Luislima07@users.noreply.github.com>
2025-08-26 14:07:29 -0300

    Mudando dependências

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express"); 
+const path = require("path"); 
+const app = express(); 
+
+// Configuração do EJS como view engine 
+app.set("views", path.join(__dirname, "views")); 
+app.set("view engine", "ejs"); 
+app.use(express.static("public"))
+// Rota principal 
+const indexRouter = require("./routes/index"); 
+app.use("/", indexRouter); 
+
+// Iniciar o servidor e sincronizar com o banco de dados 
+app.listen(3000, () => {
+console.log("Servidor em execução na porta 3000"); 
+}); 
