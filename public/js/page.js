cd69dd81ca2132ee1db567ae047afafbede7d70e
Luislima07 <161834927+Luislima07@users.noreply.github.com>
2025-08-26 15:08:29 -0300

    JS adicionado

---
+++ public/js/page.js
@@ -0,0 +1,8 @@
+document.getElementById("btnMapa").onclick = function () {
+    const iframe = document.getElementById("iframeMapa");
+    if (iframe.style.display === "none" || !iframe.style.display) {
+        iframe.style.display = "block";
+    } else {
+        iframe.style.display = "none";
+    }
+}
